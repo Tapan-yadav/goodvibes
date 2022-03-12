d151d482cdda6d1d5bf69b224edc1cd7f01aa2fe
Tapan <tapanyadav00001@gmail.com>
2022-03-12 16:59:43 +0530

    third

--- web/settings.py
+++ web/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'tapanyadav'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['goodvibesexhibitions.herokuapp.com']
 
 
 # Application definition
@@ -121,4 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /'static'
+STATIC_ROOT = os.join(BASE_DIR,'staticfiles')
